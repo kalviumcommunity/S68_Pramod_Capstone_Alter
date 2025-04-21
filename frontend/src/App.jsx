c4e1b75e0a1be1fb99451533f017ace75acf8849
kora <pramod.magesh@gmail.com>
2025-04-21 22:04:27 +0530

    Initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/login/Login"
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</>
+	)
+}
+
+export default App
