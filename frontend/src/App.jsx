b9bdcb9500cf284f95913436ede3750d905be6b6
Pramod Magesh <pramodmagesh20@gmail.com>
2025-04-22 10:11:28 +0530

    Created Login and Sign Up Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Login from "./pages/login/Login"
+import SignUp from './pages/signUp/SignUp'
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/login" element={<Login />} />
+				<Route path="/signUp" element={<SignUp />} />
 			</Routes>
 		</>
 	)
---
+++ frontend/src/pages/signUp/SignUp.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./SignUp.css"
+
+function SignUp() {
+    
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        console.log("Sign Up Action Successful");
+        alert("Sign Up Action Successful");
+    }
+    
+    return (
+        <>
+            <div className="signUpMain">
+                <form onSubmit={submitHandler} className="signUpForm">
+                    <label> Email: </label>
+                    <input type="text" name="email" id="signUpInputs" />
+
+                    <label> Password </label>
+                    <input type="password" name="password" id="signUpInputs" />
+                    
+                    <label> Re-Enter Password </label>
+                    <input type="password" id="signUpInputs"/>
+
+                    <label> Mobile Number </label>
+                    <input type="tel" name="phone" pattern="[0-9]{10}" id="signUpInputs" />
+
+                    <button type="submit"> Sign Up </button>
+
+                    
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default SignUp
