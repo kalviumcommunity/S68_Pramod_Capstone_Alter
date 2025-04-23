a6f440eaee44ec881578012f6af6cb7ea29d3ea0
Pramod Magesh <pramodmagesh20@gmail.com>
2025-04-23 14:03:12 +0530

    Netlify Test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Login from "./pages/login/Login"
 import SignUp from './pages/signUp/SignUp'
+import Navbar from './components/navbar/navbar'
 
 function App() {
 	return (
 		<>
+		<Navbar />
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/sign-up" element={<SignUp />} />
