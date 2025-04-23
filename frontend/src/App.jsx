4a5ccd20184d5b0b9b964ed2a99833713de60e85
Pramod Magesh <pramodmagesh20@gmail.com>
2025-04-23 09:06:15 +0530

    button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom"
 import Login from "./pages/login/Login"
 import SignUp from './pages/signUp/SignUp'
 
 function App() {
+
+	const navigate = useNavigate();
+
 	return (
 		<>
+		<button onClick={() => {navigate("/login")}}> Login </button>
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/sign-up" element={<SignUp />} />
