d82d4ed396cdbcbe334f7c5b4f26f7edcb2365a0
Pramod <53878629+kora07@users.noreply.github.com>
2025-04-23 11:37:09 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,8 @@ import Login from "./pages/login/Login"
 import SignUp from './pages/signUp/SignUp'
 
 function App() {
-
-	const navigate = useNavigate();
-
 	return (
 		<>
-		<button onClick={() => {navigate("/login")}}> Login </button>
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/sign-up" element={<SignUp />} />
